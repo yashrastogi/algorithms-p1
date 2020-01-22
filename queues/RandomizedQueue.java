043c14ae3cf467962b28180410650005695025e8
Yash <yashrastogi@gmail.com>
2020-01-22 19:06:14 +0530

    Queues 2 - RandomizedQueue

--- queues/RandomizedQueue.java
+++ queues/RandomizedQueue.java
@@ -1,111 +1,113 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    // unit testing code
+    private Item[] arr;
+    private int numElem;
 
-    // construct an empty randomized queue
     public RandomizedQueue() {
-
+        arr = (Item[]) new Object[10];
+        numElem = 0;
     }
 
-    // unit testing (required)
     public static void main(String[] args) {
-
+        RandomizedQueue<String> test = new RandomizedQueue<>();
+        test.enqueue("Hello1");
+        test.enqueue("World2");
+        System.out.println(test.dequeue());
+        System.out.println(test.dequeue());
+        System.out.println(test.dequeue());
     }
 
-    // is the randomized queue empty?
-    public boolean isEmpty() {
-        return false;
-    }
-
-    // return the number of items on the randomized queue
-    public int size() {
-        return 0;
-    }
-
-    // add the item
     public void enqueue(Item item) {
-
+        if (item == null) throw new IllegalArgumentException();
+        if (numElem == arr.length) {
+            resize(1);
+        }
+        arr[numElem++] = item;
     }
 
-    // remove and return a random item
     public Item dequeue() {
-        return null;
+        if (numElem == 0) throw new NoSuchElementException();
+        int randomPos = StdRandom.uniform(numElem);
+        // System.out.print("Random: " + randomPos + " Length: "+arr.length);
+        Item temp = arr[randomPos];
+        // System.out.print(" Temp: " + temp + " NumElem: "+ numElem);
+        arr[randomPos] = arr[numElem-- - 1];
+        arr[numElem] = null;
+        // System.out.print("-"+numElem+" arrRP: " + arr[randomPos]+"   ");
+        if (arr.length / 3 == numElem) {
+            resize(0);
+        }
+        return temp;
     }
 
-    // return a random item (but do not remove it)
+    private void resize(int op) {
+        if (op == 1) {
+            Item[] doubleItems = (Item[]) new Object[arr.length * 2];
+            int j = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] != null) doubleItems[i] = arr[j++];
+            }
+            arr = doubleItems;
+        }
+        else if (op == 0) {
+            Item[] halfItems = (Item[]) new Object[arr.length / 2];
+            int j = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] != null) halfItems[i] = arr[j++];
+            }
+            arr = halfItems;
+        }
+        else {
+            throw new UnsupportedOperationException("no such op: " + op);
+        }
+    }
+
+    public boolean isEmpty() {
+        return numElem == 0;
+    }
+
+    public int size() {
+        return numElem;
+    }
+
     public Item sample() {
-        return null;
+        if (numElem == 0) throw new NoSuchElementException();
+        int randomPos = StdRandom.uniform(numElem);
+        return arr[randomPos];
     }
 
-    // return an independent iterator over items in random order
+    // public Item pop(Item item) {
+    //     if (arr.length / 3 == numElem) {
+    //         resize(0);
+    //     }
+    //     return arr[numElem--];
+    // }
+
     public Iterator<Item> iterator() {
-        return null;
+        return new RandomizedQueueIterator();
     }
 
-    private class Node<Item> {
-        private Node<Item> next;
-        private Item item;
-    }
-    private class LinkedList<Item> implements Iterable<Item> {
-        private Node<Item> first = null;
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        private int currPos = 0;
 
-        public void push(Item item) {
-            Node<Item> oldfirst = first;
-            first = new Node<Item>();
-            first.item = item;
-            first.next = oldfirst;
+        public boolean hasNext() {
+            return !(currPos >= arr.length);
         }
 
-        public Item pop() {
-            Node<Item> oldfirst = first;
-            first = first.next;
-            return oldfirst.item;
-        }
-
-        public Item removeLast() {
-            Node<Item> current = first;
-            while (current.next.next != null) {
-                current = current.next;
-            }
-            Item item = current.next.item;
-            current.next = null;
-            return item;
-        }
-
-        public Iterator<Item> iterator() {
-            return new LinkedListIterator();
-        }
-
-        public void setLast(Item item) {
-            Node<Item> current = first;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = new Node<Item>();
-            current.next.item = item;
-        }
-
-        public boolean isEmpty() {
-            return first == null;
-        }
-
-        private class LinkedListIterator implements Iterator<Item> {
-            private Node<Item> current = first;
-
-            public boolean hasNext() {
-                return current != null;
-            }
-
-            public Item next() {
-                if (!hasNext()) throw new NoSuchElementException();
-                Item item = current.item;
-                current = current.next;
-                return item;
-            }
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return arr[currPos++];
         }
     }
-
-
 }
