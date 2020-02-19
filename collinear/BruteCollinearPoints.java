ec7a3f2b9bfa28244d0055a9ba4d602070ac3262
Yash <yashrastogi@gmail.com>
2020-02-19 12:28:54 +0530

    Collinear: FCP Progress 1

--- collinear/BruteCollinearPoints.java
+++ collinear/BruteCollinearPoints.java
@@ -4,24 +4,25 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class BruteCollinearPoints {
-    List<LineSegment> segments;
-    int numSegments;
+    private List<LineSegment> segments;
 
     public BruteCollinearPoints(Point[] points) {
         Arrays.sort(points);
         segments = new ArrayList<>();
-        numSegments = 0;
         for (int i=0; i<points.length; i++) {
             for (int j=i+1; j<points.length; j++) {
                 for (int k=j+1; k<points.length; k++) {
                     for (int l=k+1; l<points.length; l++) {
                         if(points[i].slopeTo(points[j]) == points[i].slopeTo(points[k]) && points[i].slopeTo(points[l]) == points[i].slopeTo(points[k])) {
-                            numSegments++;
                             segments.add(new LineSegment(points[i], points[l]));
                         }
                     }
@@ -31,15 +32,40 @@ public class BruteCollinearPoints {
     }
 
     public static void main(String[] args) {
-        Point points[] = new Point[5];
-        points[0] = new Point(3,3);
-        points[1] = new Point(1,1);
-        points[2] = new Point(2,2);
-        points[3] = new Point(4,4);
-        points[4] = new Point(1,2);
-        BruteCollinearPoints bcp = new BruteCollinearPoints(points);
-        for(LineSegment seg : bcp.segments) {
-            System.out.println(seg);
+
+        // read the n points from a file
+        In in = new In("rs1423.txt");
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
         }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
+
+    LineSegment[] segments() {
+        return segments.toArray(new LineSegment[segments.size()]);
+    }
+
+    int numberOfSegments() {
+        return segments.size();
     }
 }
--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -4,8 +4,82 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class FastCollinearPoints {
+    private List<LineSegment> segments;
+
+    public FastCollinearPoints(Point[] points) {
+        segments = new ArrayList<LineSegment>();
+        for (int i = 0; i < points.length; i++) {
+            // int i=0;
+            final Point currPoint = points[i];
+            Arrays.sort(points, (o1, o2) -> currPoint.slopeOrder().compare(o1, o2));
+            List<Point> eqSlopePts = new ArrayList<Point>();
+            for (int j = 0; j < points.length - 1; j++) {
+                if (currPoint.slopeTo(points[j + 1]) == currPoint.slopeTo(points[j])) {
+                    eqSlopePts.add(points[j + 1]);
+                    if (eqSlopePts.isEmpty()) eqSlopePts.add(points[j]);
+                    if (j == points.length - 2 && eqSlopePts.size() > 2) {
+                        Collections.sort(eqSlopePts);
+                        segments.add(
+                                new LineSegment(currPoint, eqSlopePts.get(eqSlopePts.size() - 1)));
+                    }
+                }
+                else {
+                    if (eqSlopePts.size() > 2) {
+                        Collections.sort(eqSlopePts);
+                        segments.add(
+                                new LineSegment(currPoint, eqSlopePts.get(eqSlopePts.size() - 1)));
+                    }
+                    eqSlopePts.clear();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
+        // read the n points from a file
+        In in = new In("rs1423.txt");
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
+
+    LineSegment[] segments() {
+        return segments.toArray(new LineSegment[segments.size()]);
+    }
+
+    int numberOfSegments() {
+        return segments.size();
     }
 }
